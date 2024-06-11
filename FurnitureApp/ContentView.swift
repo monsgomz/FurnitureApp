8d4d0e9ccb1a11e6813c7f14ec6e11f54f3c0779
Montserrat Gomez <montserrat.ga95@gmail.com>
2024-06-11 15:22:59 -0600

    Set up project

--- FurnitureApp/ContentView.swift
+++ FurnitureApp/ContentView.swift
@@ -9,13 +9,38 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+		NavigationStack {
+			VStack {
+				HStack{
+					Image(systemName: "line.3.horizontal.circle")
+						.resizable()
+						.foregroundStyle(.accent)
+						.scaledToFit()
+						.frame(height: 30)
+					Spacer()
+					Image(systemName: "person.circle")
+						.resizable()
+						.foregroundStyle(.accent)
+						.scaledToFit()
+						.frame(height: 30)
+				}
+				.padding(.horizontal)
+				
+				Text("Find the \nBest ")
+					.font(.custom("Plus Jakarta Sans ExtraLight", size: 25))
+					.foregroundStyle(.accent)
+				+ Text("Furniture!")
+					.font(.custom("Plus Jakarta Sans Bold", size: 25))
+					.foregroundStyle(.accent)
+					
+				
+					
+					
+				
+			   
+			}
+			.padding()
+		}
     }
 }
 
