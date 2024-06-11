a9d0e5932fa03a07b9eceadf4c4f1e60d97f3d5c
Montserrat Gomez <montserrat.ga95@gmail.com>
2024-06-11 13:47:52 -0600

    Initial Commit

---
+++ FurnitureApp/FurnitureAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FurnitureAppApp.swift
+//  FurnitureApp
+//
+//  Created by Montserrat Gomez on 2024-06-11.
+//
+
+import SwiftUI
+
+@main
+struct FurnitureAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
