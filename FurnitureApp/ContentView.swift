2c52950b39861449c8144fbc6e7bbb4d113d15de
Montserrat Gomez <montserrat.ga95@gmail.com>
2024-06-12 15:20:25 -0600

    Add custom Tab Bar

--- FurnitureApp/ContentView.swift
+++ FurnitureApp/ContentView.swift
@@ -8,35 +8,62 @@
 import SwiftUI
 
 struct ContentView: View {
-	
+	@State var selectedTab = 0
 	
 	var body: some View {
 		
-		TabView{
-			
-			HomeView()
-				.tabItem {
-					Label("Home", systemImage: "house")
-				}
+		ZStack(alignment: .bottom){
+			TabView(selection: $selectedTab){
 				
-			FavView()
-				.tabItem {
-					Label("Favorite", systemImage: "heart.fill")
-				}
+				HomeView()
+					.tag(0)
+//					.tabItem {
+//						Label("Home", systemImage: "house")
+//					}
+					.toolbar(.hidden, for: .tabBar)
+					
+				FavView()
+					.tag(1)
+//					.tabItem {
+//						Label("Favorite", systemImage: "heart.fill")
+//					}
+					.toolbar(.hidden, for: .tabBar)
+				ShopView()
+					.tag(2)
+//					.tabItem {
+//						Label("Shop", systemImage: "cart")
+//					}
+				
+				ProfileView()
+					.tag(3)
+//					.tabItem {
+//						Label("Profile", systemImage: "person.circle")
+//					}
+					.toolbar(.hidden, for: .tabBar)
+			}
 			
-			ShopView()
-				.tabItem {
-					Label("Shop", systemImage: "cart")
+			ZStack{
+				HStack{
+					ForEach((TabbedItems.allCases), id: \.self){ item in
+						Button{
+							selectedTab = item.rawValue
+						} label: {
+							CustomTabItem(imageName: item.iconName, title: item.title, isActive: (selectedTab == item.rawValue))
+						}
+					}
 				}
-			
-			ProfileView()
-				.tabItem {
-					Label("Profile", systemImage: "person.circle")
-				}
+				.padding(6)
+			}
+			.frame(height: 70)
+			.background(.accent.opacity(0.6))
+			.cornerRadius(35)
+			.padding(.horizontal, 26)
 		}
 		
 		
+		
 	}
+	
 	
 }
 
@@ -45,3 +72,25 @@ struct ContentView: View {
 		.environment(FurnitureModelView())
 }
 
+//Para leer los valores y retornar la tabbar
+extension ContentView {
+	func CustomTabItem(imageName: String, title: String, isActive: Bool) -> some View{
+		HStack(spacing: 10){
+			Spacer()
+			Image(systemName: imageName)
+				.resizable()
+				.renderingMode(.template)
+				.foregroundColor(isActive ? .white : .brown)
+				.frame(width: 25, height: 20)
+			if isActive{
+				Text(title)
+					.font(.custom("Plus Jakarta Sans Bold", size: 16))
+					.foregroundColor(isActive ? .white : .accent)
+			}
+			Spacer()
+		}
+		.frame(width: isActive ? .infinity : 60, height: 60)
+		.background(isActive ? .accent.opacity(1) : .clear)
+		.cornerRadius(30)
+	}
+}
--- FurnitureApp/Resource/TabBarOptions.swift
+++ FurnitureApp/Resource/TabBarOptions.swift
@@ -3,9 +3,16 @@
 //  FurnitureApp
 //
 //  Created by Montserrat Gomez on 2024-06-12.
-//
+// https://medium.com/geekculture/custom-tabbar-in-swiftui-4d239410ee73
+
 
 import Foundation
+
+/*
+ Int - means all cases have integer values
+ CaseIterable - means you need to iterate over enum cases
+ = 0 means start index from zero.
+ */
 
 enum TabbedItems: Int, CaseIterable{
 	case home = 0
